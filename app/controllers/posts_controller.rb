14bede21e91af8ab0e99750d2635f240b2b50d4c
Jonathan Luk <jluk87@gmail.com>
2019-04-30 21:40:55 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,18 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    if params[:author_id]
+      @author = Author.find_by(id: params[:author_id])
+      if !@author.nil?
+        @post = @author.posts.find_by(id: params[:id])
+        redirect_to author_posts_path(@author), alert: "Post not found." if @post.nil?
+      else
+        redirect_to authors_path, alert: "Author not found."
+      end
+    else
+      @post = Post.find(params[:id])
+    end
+  
   end
 
   private
